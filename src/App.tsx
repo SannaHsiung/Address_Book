c9513439761ea8e41ffae6540bbd6334e0747260
sannahsiung <sanna.hsiung@gmail.com>
2026-05-23 15:51:26 +0200

    finished user cards

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,27 @@ export default function App() {
   }, []);
 
   return (
-    <ul>
-      {users.map((user: User) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-    </ul>
+    <div className="m-8">
+      <h1 className="text-xl">Användare</h1>
+      <ul className="grid grid-cols-1 laptop:grid-cols-4 gap-8 justify-items-center mt-4">
+        {users.map((user: User) => (
+          <li className="w-full p-4 shadow rounded" key={user.id}>
+            <div className="flex">
+              <img
+                className="w-12 h-12 rounded-full mr-4"
+                src={user.avatar}
+                alt=""
+              />
+              <div className="flex flex-col">
+                <p className="font-semibold">{user.name}</p>
+                <p className="text-gray-500">{user.email}</p>
+                <p>Phone {user.phone}</p>
+              </div>
+            </div>
+            <div className="flex justify-between"></div>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
