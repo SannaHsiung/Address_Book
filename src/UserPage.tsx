7d7bc9a7309b1bc282674c5e6e1cfb346a1fa335
sannahsiung <sanna.hsiung@gmail.com>
2026-05-25 00:41:57 +0200

    reworked UserPage UI

--- src/UserPage.tsx
+++ src/UserPage.tsx
@@ -11,7 +11,7 @@ export default function UserPage() {
 
   return (
     <div
-      className={`${darkMode ? "dark" : ""} flex h-screen w-full bg-gray-300 dark:bg-slate-900 justify-center items-center`}
+      className={`${darkMode ? "dark" : ""} flex h-screen w-full bg-gray-100 dark:bg-slate-900 justify-center items-center`}
     >
       <div className="text-center">
         <Link to="/">
@@ -35,52 +35,51 @@ export default function UserPage() {
         <h1 className="mt-6 mb-4 text-2xl font-bold dark:text-white">
           Information
         </h1>
-        <div className="p-5 rounded shadow-xl w-96 bg-white dark:bg-slate-600 ">
-          <div className="grid place-items-center">
-            <img src={user.avatar} className="rounded mb-4" alt="profile-img" />
-          </div>
-          <h1 className="text-xl font-bold dark:text-white">{user.name}</h1>
-          <div className="grid text-left ml-16">
+
+        <div className="grid place-items-center">
+          <img src={user.avatar} className="rounded mb-4" alt="profile-img" />
+        </div>
+        <h1 className="text-xl font-bold dark:text-white">{user.name}</h1>
+        <div className="grid text-left ml-2">
+          <p>
+            <i className="fa-solid fa-envelope mr-2 dark:text-white" />
+            <span className="dark:text-white">{user.email}</span>
+          </p>
+          <p>
+            <i className="fa-solid fa-mobile-screen mr-2 dark:text-white" />
+            <span className="dark:text-white">{user.phone}</span>
+          </p>
+        </div>
+        <div className="mt-8">
+          <h1 className="font-bold -ml-6">
+            <i className="fa-solid fa-location-dot mr-1 dark:text-white" />
+            <span className="dark:text-white">Adress</span>
+          </h1>
+          <div className="grid text-left ml-8">
             <p>
-              <i className="fa-solid fa-envelope mr-2 dark:text-white" />
-              <span className="dark:text-white">{user.email}</span>
+              <span className="font-semibold dark:text-white">Gata: </span>
+              <span className="dark:text-white">{`${user.streetNumber} ${user.streetName}`}</span>
             </p>
             <p>
-              <i className="fa-solid fa-mobile-screen mr-2 dark:text-white" />
-              <span className="dark:text-white">{user.phone}</span>
+              <span className="font-semibold dark:text-white">Stad: </span>
+              <span className="dark:text-white">{user.city}</span>
+            </p>
+            <p>
+              <span className="font-semibold dark:text-white">Delstat: </span>
+              <span className="dark:text-white">{user.state}</span>
+            </p>
+            <p>
+              <span className="font-semibold dark:text-white">Land: </span>
+              <span className="dark:text-white">{user.country}</span>
             </p>
           </div>
-          <div className="mt-8">
-            <h1 className="font-bold">
-              <i className="fa-solid fa-location-dot mr-1 dark:text-white" />
-              <span className="dark:text-white">Adress</span>
-            </h1>
-            <div className="grid text-left ml-24">
-              <p>
-                <span className="font-semibold dark:text-white">Gata: </span>
-                <span className="dark:text-white">{`${user.streetNumber} ${user.streetName}`}</span>
-              </p>
-              <p>
-                <span className="font-semibold dark:text-white">Stad: </span>
-                <span className="dark:text-white">{user.city}</span>
-              </p>
-              <p>
-                <span className="font-semibold dark:text-white">Delstat: </span>
-                <span className="dark:text-white">{user.state}</span>
-              </p>
-              <p>
-                <span className="font-semibold dark:text-white">Land: </span>
-                <span className="dark:text-white">{user.country}</span>
-              </p>
-            </div>
-          </div>
-          <div className="mt-8">
-            <h1 className="font-bold">
-              <i className="fa-solid fa-file-pen mr-2 dark:text-white" />
-              <span className="dark:text-white">Anställningsdatum</span>
-            </h1>
-            <p className="dark:text-white">{user.registered}</p>
-          </div>
+        </div>
+        <div className="mt-8">
+          <h1 className="font-bold">
+            <i className="fa-solid fa-file-pen mr-2 dark:text-white" />
+            <span className="dark:text-white">Anställningsdatum</span>
+          </h1>
+          <p className="dark:text-white">{user.registered}</p>
         </div>
       </div>
     </div>
